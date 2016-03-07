0320c92aa2757ac79e35be068d7ce35fe2c918b0
Xueyongwei <eoe_dev@icloud.com>
2016-03-07 15:29:20 +0800

    Initial Commit

---
+++ XsingleRotationGestureRecognizer-Master/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XsingleRotationGestureRecognizer-Master
+//
+//  Created by xueyongwei on 16/3/7.
+//  Copyright © 2016年 xueyongwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
